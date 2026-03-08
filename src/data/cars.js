4c35b6f0a54d000ed8ec9339dd8d66474995ee30
NicolasSs1203 <senketsu1203456@protonmail.com>
2026-03-08 18:03:26 -0500

    feat: initial commit of CARROS MOTORS Astro platform

---
+++ src/data/cars.js
@@ -0,0 +1,83 @@
+export const cars = [
+  {
+    id: 1,
+    make: "Subaru",
+    model: "WRX STI",
+    year: 2004,
+    price: "$136.500.000",
+    specs: "300 hp • 0-60 4.6s • Velocidad máxima 248 km/h",
+    image: "/imagenes/carros/Subaru-Impreza_WRX_STi-2004-Front_Three-Quarter.6c2f7bfa.jpg"
+  },
+  {
+    id: 2,
+    make: "Toyota",
+    model: "Corolla Sedan",
+    year: 2024,
+    price: "$114.900.000",
+    specs: "169 hp • 0-60 8.2s • Velocidad máxima 188 km/h",
+    image: "/imagenes/carros/Toyota-Corolla_Sedan_EU-Version-2019-wallpaper.jpg"
+  },
+  {
+    id: 3,
+    make: "Chevrolet",
+    model: "Corvette Z06",
+    year: 2024,
+    price: "$439.500.000",
+    specs: "670 hp • 0-60 2.6s • Velocidad máxima 304 km/h",
+    image: "/imagenes/carros/Chevrolet-Corvette_Z06-2023-Front_Three-Quarter.bbfde54d.jpg"
+  },
+  {
+    id: 4,
+    make: "Chevrolet",
+    model: "Cruze",
+    year: 2016,
+    price: "$56.000.000",
+    specs: "153 hp • 0-60 7.7s • Velocidad máxima 201 km/h",
+    image: "/imagenes/carros/Chevrolet-Cruze-2016-Front_Three-Quarter.729308c5.jpg"
+  },
+  {
+    id: 5,
+    make: "Mercedes-AMG",
+    model: "GT R",
+    year: 2021,
+    price: "$616.200.000",
+    specs: "577 hp • 0-60 3.5s • Velocidad máxima 318 km/h",
+    image: "/imagenes/carros/Mercedes-Benz-AMG_GT63_S_E_Performance-2025-Front_Three-Quarter.45cfd2d0.jpg"
+  },
+  {
+    id: 6,
+    make: "McLaren",
+    model: "750S",
+    year: 2024,
+    price: "$1.263.600.000",
+    specs: "740 hp • 0-60 2.7s • Velocidad máxima 332 km/h",
+    image: "/imagenes/carros/McLaren-750S-2024-HD.jpg"
+  },
+  {
+    id: 7,
+    make: "Porsche",
+    model: "911 GT3",
+    year: 2024,
+    price: "$713.300.000",
+    specs: "502 hp • 0-60 3.2s • Velocidad máxima 318 km/h",
+    image: "/imagenes/carros/Porsche-911_GT3-2014-wallpaper.jpg"
+  },
+  {
+    id: 8,
+    make: "Nissan",
+    model: "GT-R",
+    year: 2014,
+    price: "$292.500.000",
+    specs: "545 hp • 0-60 2.9s • Velocidad máxima 315 km/h",
+    image: "/imagenes/carros/Nissan-GT-R-2014-wallpaper.jpg"
+  },
+  {
+    id: 10,
+    make: "BMW",
+    model: "M3 E30",
+    year: 1990,
+    price: "$275.000.000",
+    specs: "192 hp • 0-60 6.7s • Velocidad máxima 235 km/h",
+    image: "/imagenes/carros/BMWE30.jpg"
+  }
+];
